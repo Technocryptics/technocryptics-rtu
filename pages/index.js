f232c0c3f234440d8bf5666f839a74cb53f7dd8a
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-15 18:02:09 +0530

    tailwind config

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
         <title>Technocryptics RTU</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className="min-h-screen w-full flex items-center justify-center text-2xl bg-emerald-100/30 text-emerald-900 font-mono font-semibold lg:text-4xl">
+        Hello India!
+      </main>
     </>
   );
 }
