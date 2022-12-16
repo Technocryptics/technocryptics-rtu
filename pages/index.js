2ee1889ae6797a87403512cd5ecb04dd84553dc3
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-16 05:56:35 +0530

    navigation bar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -7,8 +8,8 @@ export default function Home() {
         <title>Technocryptics RTU</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen w-full flex items-center justify-center text-2xl bg-emerald-100/30 text-emerald-900 font-mono font-semibold lg:text-4xl">
-        Hello India!
+      <main className="bg-white text-black min-h-screen">
+        <Navbar />
       </main>
     </>
   );
