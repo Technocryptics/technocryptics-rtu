ec56971b06f3ba3ed0caf3586c91133e5c8b04b8
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-16 22:35:25 +0530

    Footer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Footer from "../components/globals/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       <main className="bg-white text-black min-h-screen">
         <Navbar />
+        <Footer />
       </main>
     </>
   );
