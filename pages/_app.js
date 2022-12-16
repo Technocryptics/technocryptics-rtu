096338c2d2c855cfe3f0d4667b794bc8e1456621
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-17 02:00:27 +0530

    custom font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/tailwind_config.css";
+import "../styles/scrollbar.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="custom-font bg-white text-black min-h-screen">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
