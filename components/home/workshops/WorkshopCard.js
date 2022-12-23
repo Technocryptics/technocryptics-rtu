962eb00511d460881897153354adc3280625ba16
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-23 19:32:09 +0530

    lazy loading

--- components/home/workshops/WorkshopCard.js
+++ components/home/workshops/WorkshopCard.js
@@ -4,7 +4,11 @@ export default function WorkshopCard({ is = "upcoming", name = "Workshop" }) {
   return (
     <div className="card w-[90%] sm:w-96 bg-slate-50 text-slate-800 border-2 border-fuchsia-100 nap-center flex-shrink-0">
       <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img
+          src="https://placeimg.com/400/225/arch"
+          loading="lazy"
+          alt="Shoes"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
