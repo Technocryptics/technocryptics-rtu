e4406f364dc31ad5742abd3872d14b1c4f93263e
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-23 18:54:31 +0530

    Footer.jsx

--- components/globals/Footer.jsx
+++ components/globals/Footer.jsx
@@ -6,6 +6,17 @@ export default function Footer() {
       <div>
         <p className="font-semibold md:text-lg"> Technocryptics</p>
         <p>A graphic designing club of Rajasthan Technical University, Kota</p>
+        <p>
+          <a
+            href="https://pushkaryadav.in/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="inline-block transition-all no-underline text-inherit hover:brightness-110 mt-2"
+            title="Website developed by Pushkar Yadav"
+          >
+            &lt;/&gt; by Pushkar Yadav
+          </a>
+        </p>
       </div>
       <div>
         <span className="footer-title">Social</span>
