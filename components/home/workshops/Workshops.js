81e31a058774c0d3b7f0abbdfd7ff39014ca3eff
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-23 19:32:26 +0530

    workshops hero text

--- components/home/workshops/Workshops.js
+++ components/home/workshops/Workshops.js
@@ -7,6 +7,12 @@ export default function Workshops() {
       <div className="text-xl md:text-2xl lg:text-3xl font-bold text-center">
         Workshops
       </div>
+      <p className="max-w-md md:max-w-lg xl:max-w-2xl 2xl:max-w-6xl text-lg mt-4 text-slate-800 mx-auto text-justify">
+        Professional workshops and various events have been organized by the
+        Techno-Cryptics Club from time to time. These workshops are organized
+        during each semester in order to help anyone interested in becoming a
+        professional graphic designer.
+      </p>
       <WorkshopCarousel />
     </div>
   );
