92e333761d80abbf6cae62c5f6fb18fc3c35d669
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-25 10:19:31 +0530

    implemented loader

---
+++ components/globals/Loader.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <>
+      <div className="frame w-full h-screen overflow-hidden">
+        <div className="center">
+          <div className="loader">
+            <div className="plane bottom"></div>
+            <div className="plane left"></div>
+            <div className="plane right"></div>
+            <div className="plane back"></div>
+            <div className="plane front"></div>
+            <div className="plane top"></div>
+            <div className="plane fill-top"></div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .center {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          perspective: 500px;
+        }
+
+        .loader {
+          width: 120px;
+          height: 120px;
+          transform-style: preserve-3d;
+          transform: rotateX(-25deg) rotateY(45deg);
+          transition: all 0.5s ease-in-out;
+          cursor: pointer;
+        }
+        .loader:hover {
+          transform: rotateX(-25deg) rotateY(45deg) translateY(-15px);
+        }
+
+        .loader .plane {
+          position: absolute;
+          width: 120px;
+          height: 120px;
+          top: 0;
+          left: 0;
+          background: rgba(0, 0, 0, 0.1);
+        }
+        .loader .bottom {
+          transform: rotateX(-90deg) translateZ(60px);
+          background: #fbbd23;
+          box-shadow: -8px -8px 10px 0 rgba(0, 0, 0, 0.1);
+          transition: all 0.5s ease-in-out;
+        }
+        .loader .left {
+          transform: rotateY(-90deg) translateZ(60px);
+        }
+        .loader .left:before {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background: #fbbd23;
+          transform-origin: 50% 100%;
+          -webkit-animation: fill 4s ease-in-out infinite alternate;
+          animation: fill 4s ease-in-out infinite alternate;
+        }
+        .loader .right {
+          transform: rotateY(90deg) translateZ(60px);
+        }
+        .loader .right:before {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background: #fbbd23;
+          transform-origin: 50% 100%;
+          -webkit-animation: fill 4s ease-in-out infinite alternate;
+          animation: fill 4s ease-in-out infinite alternate;
+        }
+        .loader .back {
+          transform: rotateY(180deg) translateZ(60px);
+        }
+        .loader .back:before {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background: #fbbd23;
+          transform-origin: 50% 100%;
+          -webkit-animation: fill 4s ease-in-out infinite alternate;
+          animation: fill 4s ease-in-out infinite alternate;
+        }
+        .loader .front {
+          transform: rotateY(0deg) translateZ(60px);
+        }
+        .loader .front:before {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background: #fbbd23;
+          transform-origin: 50% 100%;
+          -webkit-animation: fill 4s ease-in-out infinite alternate;
+          animation: fill 4s ease-in-out infinite alternate;
+        }
+        .loader .top {
+          transform: rotateX(90deg) translateZ(60px);
+          opacity: 0.5;
+        }
+        .loader .fill-top {
+          background: #fbbd23;
+          -webkit-animation: fill-top 4s ease-in-out infinite alternate;
+          animation: fill-top 4s ease-in-out infinite alternate;
+        }
+
+        @-webkit-keyframes fill {
+          0%,
+          25% {
+            transform: scaleY(0);
+          }
+          75%,
+          100% {
+            transform: scaleY(1);
+          }
+        }
+
+        @keyframes fill {
+          0%,
+          25% {
+            transform: scaleY(0);
+          }
+          75%,
+          100% {
+            transform: scaleY(1);
+          }
+        }
+        @-webkit-keyframes color-bottom {
+          0% {
+            background: rgba(0, 0, 0, 0.1);
+          }
+          25%,
+          100% {
+            background: #fbbd23;
+          }
+        }
+        @keyframes color-bottom {
+          0% {
+            background: rgba(0, 0, 0, 0.1);
+          }
+          25%,
+          100% {
+            background: #fbbd23;
+          }
+        }
+        @-webkit-keyframes color-top {
+          0%,
+          25% {
+            background: rgba(0, 0, 0, 0.1);
+          }
+          75%,
+          100% {
+            background: #fbbd23;
+          }
+        }
+        @keyframes color-top {
+          0%,
+          25% {
+            background: rgba(0, 0, 0, 0.1);
+          }
+          75%,
+          100% {
+            background: #fbbd23;
+          }
+        }
+        @-webkit-keyframes fill-top {
+          0%,
+          25% {
+            transform: rotateX(90deg) translateZ(-60px);
+          }
+          75%,
+          100% {
+            transform: rotateX(90deg) translateZ(59px);
+          }
+        }
+        @keyframes fill-top {
+          0%,
+          25% {
+            transform: rotateX(90deg) translateZ(-60px);
+          }
+          75%,
+          100% {
+            transform: rotateX(90deg) translateZ(59px);
+          }
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/workshops/[workshop].js
+++ pages/workshops/[workshop].js
@@ -1,8 +1,26 @@
 import { useRouter } from "next/router";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Loader from "../../components/globals/Loader";
+import workshops from "./../../config/workshops.json";
 export default function Workshops() {
   const router = useRouter();
   const { workshop } = router.query;
-  return <div>Workshops: {workshop}</div>;
+  const [data, setData] = useState(null);
+  // check if router data is loaded
+  useEffect(() => {
+    if (workshop != undefined) {
+      if (workshop in workshops) {
+        let _data = workshops[workshop];
+        // setData(_data);
+      } else {
+        router.push("/");
+      }
+    }
+  });
+  console.log(data);
+  return (
+    <>
+      <Loader />
+    </>
+  );
 }
