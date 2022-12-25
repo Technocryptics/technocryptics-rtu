c210a0d5d159b90b85be546121540c45228db674
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-25 18:45:34 +0530

    meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,41 @@ export default function Document() {
     <Html lang="en" className="scroll-p-32 xl:scroll-p-36  2xl:scroll-p-48">
       <Head>
         <title>Technocryptics</title>
+        {/* meta tags generated from WebSearcher(https://websearcher.pushkaryadav.in/) */}
+        <meta name="title" content="Technocryptics" />
+        <meta
+          name="description"
+          content="A graphic designing club of Rajasthan Technical University, Kota"
+        />
+        <meta name="keywords" content="" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Pushkar Yadav" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://technocryptics.vercel.app/" />
+        <meta property="og:title" content="Technocryptics" />
+        <meta
+          property="og:description"
+          content="A graphic designing club of Rajasthan Technical University, Kota"
+        />
+        <meta
+          property="og:image"
+          content="https://technocryptics.vercel.app/banner.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://technocryptics.vercel.app/"
+        />
+        <meta property="twitter:title" content="Technocryptics" />
+        <meta
+          property="twitter:description"
+          content="A graphic designing club of Rajasthan Technical University, Kota"
+        />
+        <meta
+          property="twitter:image"
+          content="https://technocryptics.vercel.app/banner.png"
+        />
       </Head>
       <body>
         <div>
