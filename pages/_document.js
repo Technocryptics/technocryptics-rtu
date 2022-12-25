b1886fb3250c7553aa958139715ae36e9e6cf942
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-25 18:44:37 +0530

    scroll padding

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="scroll-p-32 xl:scroll-p-36  2xl:scroll-p-48">
       <Head>
         <title>Technocryptics</title>
       </Head>
