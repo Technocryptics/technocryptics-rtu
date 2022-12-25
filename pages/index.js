8c00b7c0c923c75fe38cf1781c60249ae67c5f9b
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-25 20:51:39 +0530

    tagline update

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Footer from "../components/globals/Footer";
 import Navbar from "../components/globals/Navbar";
 import Hero from "../components/home/Hero";
+import Tagline from "../components/home/Tagline";
 import Workshops from "../components/home/workshops/Workshops";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Hero />
+      <Tagline />
       <Workshops />
       <Footer />
     </>
