fe61fdd9aa7a484075c25d44f4820208a05f4924
Puskar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-25 19:13:04 +0530

    Footer link updates

--- components/globals/Navbar.jsx
+++ components/globals/Navbar.jsx
@@ -27,13 +27,24 @@ export default function Navbar() {
             className="menu menu-compact dropdown-content mt-3 p-2 ring-2 ring-amber-100 rounded-box w-52 bg-white"
           >
             <li>
-              <a className="active:bg-amber-400">Home</a>
+              <Link href="/" className="active:bg-amber-400">
+                Home
+              </Link>
             </li>
             <li>
-              <a className="active:bg-amber-400">Workshops</a>
+              <Link className="active:bg-amber-400" href="/#workshop">
+                Workshops
+              </Link>
             </li>
             <li>
-              <a className="active:bg-amber-400">Links</a>
+              <a
+                target="_blank"
+                rel="noreferrer noopener"
+                className="active:bg-amber-400"
+                href="https://linktr.ee/technocryptics"
+              >
+                Linktree
+              </a>
             </li>
           </ul>
         </div>
@@ -47,7 +58,12 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
+        <button
+          className="btn btn-ghost btn-circle"
+          onClick={() => {
+            alert("Check our amazing workshops!");
+          }}
+        >
           <div className="indicator">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -63,7 +79,6 @@ export default function Navbar() {
                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />
             </svg>
-            {/* Show on basis of state if a first user or not:  <span className="badge badge-xs badge-primary indicator-item"></span> */}
           </div>
         </button>
       </div>
